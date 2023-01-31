abe2b60f20c96ff8d4050e536e24165aa6a422d4
Leejiyong <ljy1110@midasin.com>
2023-01-31 22:47:36 +0900

    [front] next 13 설치

---
+++ web/src/app/layout.tsx
@@ -0,0 +1,14 @@
+import './globals.css';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang='ko'>
+      <head />
+      <body>{children}</body>
+    </html>
+  );
+}
