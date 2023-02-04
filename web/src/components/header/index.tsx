e7f88980cf172f1a9e9a9c9bc8f34de66f534b6d
jiyong LEE <49055628+Jiyong95@users.noreply.github.com>
2023-02-04 13:31:16 +0900

    [front]feature : header logo추가

--- web/src/components/header/index.tsx
+++ web/src/components/header/index.tsx
@@ -1,15 +1,22 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import styles from '@styles/header.module.scss';
 
 const Header = () => {
   return (
     <header className={styles.header}>
-      <div className={styles.logo}>LOGO</div>
-      <div className={styles.login_Wrap}>
-        <Link href='/login' className=''>
-          Login
-        </Link>
-      </div>
+      <Link href="/">
+        <Image
+          src="/img/header/logo.svg"
+          alt="13"
+          width={40}
+          height={40}
+          priority
+        />
+      </Link>
+      <Link href="/login" className="">
+        Login
+      </Link>
     </header>
   );
 };
