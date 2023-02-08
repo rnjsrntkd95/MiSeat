85ad41bae391d09564527181bfa69dfa4f32c39b
rnjsrntkd95 <kks1023@midasin.com>
2023-02-08 23:58:27 +0900

    feat: @Getter 추가

--- server/src/main/java/com/miseat/entity/enums/SeatType.java
+++ server/src/main/java/com/miseat/entity/enums/SeatType.java
@@ -1,12 +1,15 @@
 package com.miseat.entity.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum SeatType {
 
     FIX("고정 좌석"),
     FREE("자율 좌석"),
     NONE("좌석 없음");
 
-    private String description;
+    private final String description;
 
     SeatType(String description) {
         this.description = description;
