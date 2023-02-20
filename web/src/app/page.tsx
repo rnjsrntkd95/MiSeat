7d54d8d6be80ff8a21624d7a11f2c5397d11d748
Leejiyong <ljy1110@midasin.com>
2023-02-20 14:16:17 +0900

    feature: weekCalendar 추가

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -2,13 +2,15 @@ import Image from 'next/image';
 import { Inter } from '@next/font/google';
 import styles from '@styles/page.module.css';
 import Calendar from '@/components/calendar';
+import WeekCalender from '@components/weekCalendar';
 
 const inter = Inter({ subsets: ['cyrillic-ext'] });
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <Calendar />
+      <WeekCalender />
+      {/* <Calendar /> */}
     </main>
   );
 }
