99ac8f859ea34042e2c05157cfc12d6fea83768a
jiyong LEE <49055628+Jiyong95@users.noreply.github.com>
2023-02-26 22:02:03 +0900

    feature:weekCalendar socket 메소드 연결

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,16 +1,15 @@
 import WeekCalender from '@components/weekCalendar';
 import { useState } from 'react';
-import * as StompJS from '@stomp/stompjs';
 import { useSpace } from '@repository/space/useSpace';
 import SocketJs from '@components/sockJs';
 
 export default function Home() {
-  const [client, setClient] = useState<StompJS.Client>();
   const [selectDate, setSelectDate] = useState<Date>(new Date());
-  const { data } = useSpace(selectDate);
+  const { data: spaceData } = useSpace(selectDate);
+
   return (
     <>
-      {/* <SocketJs client={client} setClient={setClient} /> */}
+      <SocketJs spaceData={spaceData} />
       <WeekCalender selectDate={selectDate} setSelectDate={setSelectDate} />
     </>
   );
--- web/src/utils/axios.ts
+++ web/src/utils/axios.ts
@@ -7,4 +7,12 @@ const customAxios: AxiosInstance = axios.create({
 customAxios.defaults.headers.common['Authorization'] =
   'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJNaVNlYXQgSXNzdWVyIiwidGVhbUNvZGUiOjc3Nzc3NywiZXhwIjoxNzc1OTU2NTY2LCJ1c2VySWQiOiJra3MxMDIzIiwiaWF0IjoxNjc1OTU2NTY2fQ.NV4llGQS22QFCCQLTpNOrnIXi0xvqTe_d-_x_3nejIo';
 
+export const getheader = () => {
+  return {
+    Authorization: customAxios.defaults.headers.common[
+      'Authorization'
+    ] as string,
+  };
+};
+
 export default customAxios;
